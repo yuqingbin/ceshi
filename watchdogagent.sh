9d5f91d889cb54ceb04e0431e5cec4c44b772f8b
yuqingbin <yuqingbingo@163.com>
2019-06-05 15:50:01 +0800

    Update watchdogagent.sh

--- watchdogagent.sh
+++ watchdogagent.sh
@@ -1,1 +1,16 @@
+#! /bin/sh
+#进程名字可修改
+PRO_NAME=nvr.out
+HOME_PATH=/home/vgs/nvr
 
+while true ; do
+  sleep 1
+  NUM=`ps aux | grep ${PRO_NAME} | grep -v grep |wc -l`
+  if [ "${NUM}" -lt "1" ];then
+    echo "${PRO_NAME} was killed"
+	cd ${HOME_PATH}
+	nohup ./${PRO_NAME} &
+  fi
+done
+
+exit 0
